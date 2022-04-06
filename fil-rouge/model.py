70af6d3c072116facdb81925ee64e1c835b91761
LuizGiserman <luizgiserman@poli.ufrj.br>
2022-04-06 18:08:21 +0200

    Refactor utilities

--- fil-rouge/model.py
+++ fil-rouge/model.py
@@ -2,13 +2,22 @@ import mesa
 from mesa.space import MultiGrid
 from datetime import datetime
 from mesa.time import RandomActivation
+from recuit import recuitAgent
+from utilities import Utilities as util
+
+new_util = util()
 
 class GlobalMASModel(mesa.Model):
   def __init__(self, agents):
     self.schedule = RandomActivation(self)
-    # self.schedule.add(ListAgent(0,M,self))
+    self.schedule.add(recuitAgent(0, new_util, t=150, max_iter=30, a=0.95, model=self))
     # self.schedule.add(Johnson2Agent(1,M,self))
     # self.evaluator = EvalAgent(0,M, self, self.schedule.agents)
   def step(self):
     self.schedule.step()
-    return self.evaluator.step([agent.eval() for agent in self.schedule.agents]).copy()
+    print(util.cost_function (self.schedule.agents[0].eval()))
+    # return self.evaluator.step([agent.eval() for agent in self.schedule.agents]).copy()
+
+model = GlobalMASModel([])
+for i in range(5):
+    model.step()
